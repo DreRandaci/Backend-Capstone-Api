80c46637a280da1c92d6058e9e2b6c6cdd5252a7
Dre Randaci <drerandaci@gmail.com>
2018-03-08 10:04:31 -0600

    user model properties

--- BackendWatsonApi/Models/User.cs
+++ BackendWatsonApi/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,18 @@ namespace BackendWatsonApi.Models
 {
     public class User
     {
+        [Key]
+        public int UserId { get; set; }
 
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
     }
 }
