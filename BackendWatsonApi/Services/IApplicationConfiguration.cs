4f8cab7b636e7219f9b9a24ed709fdb1e5b2f9c0
Dre Randaci <drerandaci@gmail.com>
2018-03-08 11:24:32 -0600

    updated appsettings

---
+++ BackendWatsonApi/Services/IApplicationConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendWatsonApi.Services
+{
+    public interface IApplicationConfiguration
+    {
+        string WatsonApiKey { get; set; }
+    }
+}
