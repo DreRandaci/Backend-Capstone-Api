2865d8de4d6ab39745415e263bf8fb3a34609485
Dre Randaci <drerandaci@gmail.com>
2018-03-08 10:14:09 -0600

    removed unnessary usings

--- BackendWatsonApi/Controllers/ValuesController.cs
+++ BackendWatsonApi/Controllers/ValuesController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using IBM.WatsonDeveloperCloud.VisualRecognition.v3;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 
 namespace BackendWatsonApi.Controllers
 {
