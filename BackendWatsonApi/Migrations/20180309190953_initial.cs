d1c767aa9c9731598dc9a07eb26d75cb95f1b00b
Dre Randaci <drerandaci@gmail.com>
2018-03-09 13:18:13 -0600

    initial migration with database update

---
+++ BackendWatsonApi/Migrations/20180309190953_initial.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BackendWatsonApi.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Place",
+                columns: table => new
+                {
+                    PlaceId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Address = table.Column<string>(nullable: false),
+                    Latitude = table.Column<double>(nullable: false),
+                    Longitude = table.Column<double>(nullable: false),
+                    Notes = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Place", x => x.PlaceId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Email = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(maxLength: 100, nullable: false),
+                    cameraFormatIsSet = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WatsonClassification",
+                columns: table => new
+                {
+                    ClassificationId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Class = table.Column<string>(nullable: false),
+                    ClassifierId = table.Column<string>(nullable: false),
+                    ClassifierName = table.Column<string>(nullable: false),
+                    ConfidenceScore = table.Column<int>(nullable: false),
+                    TypeHierarchy = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WatsonClassification", x => x.ClassificationId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Image",
+                columns: table => new
+                {
+                    ImageId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ClassificationId = table.Column<int>(nullable: false),
+                    DateAdded = table.Column<DateTime>(nullable: false),
+                    ImageByteData = table.Column<string>(nullable: false),
+                    PlaceId = table.Column<int>(nullable: false),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Image", x => x.ImageId);
+                    table.ForeignKey(
+                        name: "FK_Image_WatsonClassification_ClassificationId",
+                        column: x => x.ClassificationId,
+                        principalTable: "WatsonClassification",
+                        principalColumn: "ClassificationId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Image_Place_PlaceId",
+                        column: x => x.PlaceId,
+                        principalTable: "Place",
+                        principalColumn: "PlaceId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Image_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Image_ClassificationId",
+                table: "Image",
+                column: "ClassificationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Image_PlaceId",
+                table: "Image",
+                column: "PlaceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Image_UserId",
+                table: "Image",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Image");
+
+            migrationBuilder.DropTable(
+                name: "WatsonClassification");
+
+            migrationBuilder.DropTable(
+                name: "Place");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
