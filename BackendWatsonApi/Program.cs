cefd45992c46c2e3e572ada9cfdb4f836914af5e
Dre Randaci <drerandaci@gmail.com>
2018-03-15 16:11:18 -0500

    0.0.0.0:5000 listening when navigating to the localhost endpoint

--- BackendWatsonApi/Program.cs
+++ BackendWatsonApi/Program.cs
@@ -15,15 +15,15 @@ namespace BackendWatsonApi
     public class Program
     {
         public static void Main(string[] args)
-        {            
-
+        {
+            
             BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseUrls("http://0.0.0:5000;https://*:5000")
+                .UseStartup<Startup>()                
+                .UseUrls("http://0.0.0.0:5000")
                 .Build();                
     }
 }
