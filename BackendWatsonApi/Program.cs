9dd0478442288bf13e22c2bcb0e708abadb5bcdf
Dre Randaci <drerandaci@gmail.com>
2018-03-15 15:44:49 -0500

    seed data

--- BackendWatsonApi/Program.cs
+++ BackendWatsonApi/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using BackendWatsonApi.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace BackendWatsonApi
@@ -14,12 +16,46 @@ namespace BackendWatsonApi
     {
         public static void Main(string[] args)
         {
+            // DO NOT REMOVE THE FOLLOWING LINE
+            var host = BuildWebHost(args);
+
+            /************************************************************************/
+            /* 1. Comment out or remove the Seed Block to stop seeding the database */
+            /* 2. Drop or delete the database (the .db file)                        */
+            /* 3. Update the database                                               */                          
+            /************************************************************************/
+
+            /********/
+            /* Seed */
+            /********/
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+            /************/
+            /* End Seed */
+            /************/
+
+            // DO NOT REMOVE THE FOLLOWING LINE
+            host.Run();
+
             BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .Build();
+                .UseUrls("http://0.0.0:5000;https://*:5000")
+                .Build();                
     }
 }
