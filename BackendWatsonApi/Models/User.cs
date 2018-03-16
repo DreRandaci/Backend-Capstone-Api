17adbc9dad2cd26b2b6d23e875a0d73b3176dc20
Dre Randaci <drerandaci@gmail.com>
2018-03-15 23:37:15 -0500

    token controller setup and new user model

--- BackendWatsonApi/Models/User.cs
+++ BackendWatsonApi/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,22 +7,16 @@ using System.Threading.Tasks;
 
 namespace BackendWatsonApi.Models
 {
-    public class User
+    public class User : IdentityUser 
     {
         [Key]
-        public int UserId { get; set; }
+        public int UserId { get; set; }        
 
         [Required]
-        [EmailAddress]
-        public string Email { get; set; }
+        public string FirstName { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }           
-        
-        [Required]
-        public string UserName { get; set; }
+        public string LastName { get; set; }
 
         public virtual ICollection<UserPost> UserPosts { get; set; }                  
     }
